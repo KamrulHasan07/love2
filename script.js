406c9f5d31740bd2af7aa40bee7de66f10cf54bf
KamrulHasan07 <88605910+KamrulHasan07@users.noreply.github.com>
2021-10-01 11:21:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("btn");
+let btn2 = document.getElementById("btn2");
+
+btn.addEventListener("click", function (e) {
+  document.write(`I LOVE YOU JEMI.`);
+});
+
+btn2.addEventListener("click", function (e) {
+  document.write("Else, I LOVE YOU");
+});
